5c65d74da689522bba725a7223375222607a35f0
kirill_masanov <kirill_masanov@mail.ru>
2020-07-11 23:02:33 +0300

    add process request

--- web_server.py
+++ web_server.py
@@ -1,11 +1,22 @@
 import socket
 
+HOST = 'localhost'  # or '' to listen on any interface
+PORT = 8080  # listen on port 8080
+MAX_SIZE = 1024
+
+
+def process_request(data):
+    print('Received data:\n', data.decode('UTF-8'), sep='')
+    return 'Hello World!'
+
+
 with socket.socket() as s:
-    s.bind(('localhost', 8080))
+    s.bind((HOST, PORT))
     s.listen(1)
-    sock, addr = s.accept()
-    with sock:
-        print('Address info:', addr)
-        data = sock.recv(1024)
-        print('Received data:\n', data.decode('UTF-8'), sep='')
-        sock.sendall(b'Hello World!')
+    while True:
+        sock, addr = s.accept()
+        with sock:
+            print('Address info:', addr)
+            request = sock.recv(MAX_SIZE)
+            response = process_request(request)
+            sock.sendall(response.encode('utf-8'))
