88d4727cc441e4711d2d5c26b9c3bc301f5c1e2c
kirill_masanov <kirill_masanov@mail.ru>
2020-07-12 16:33:33 +0300

    parsing request

--- web_server.py
+++ web_server.py
@@ -1,3 +1,4 @@
+import json
 import socket
 
 HOST = 'localhost'  # or '' to listen on any interface
@@ -5,9 +6,30 @@ PORT = 8080  # listen on port 8080
 MAX_SIZE = 1024
 
 
+def parse_header_line(line):
+    print(line)
+    key, value = line.split(':', maxsplit=1)
+    return key, value
+
+
+def parse_http(data):
+    request_line, *header_lines = data.split('\r\n')
+    method, path, protocol = request_line.split()
+    headers = dict(
+        parse_header_line(line)
+        for line in header_lines
+    )
+    return {
+        'method': method,
+        'path': path,
+        'protocol': protocol,
+        'headers': headers,
+    }
+
+
 def process_request(data):
-    print('Received data:\n', data.decode('UTF-8'), sep='')
-    return 'Hello World!'
+    print(json.dumps(request, indent=4))
+    return f'Hello {request["path"]}'
 
 
 with socket.socket() as s:
@@ -17,6 +39,7 @@ with socket.socket() as s:
         sock, addr = s.accept()
         with sock:
             print('Address info:', addr)
-            request = sock.recv(MAX_SIZE)
+            request_data = sock.recv(MAX_SIZE).decode('utf-8').strip()
+            request = parse_http(request_data)
             response = process_request(request)
             sock.sendall(response.encode('utf-8'))
